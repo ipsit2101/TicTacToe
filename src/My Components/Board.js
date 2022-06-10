63678ffc4fc3b4c2212ada015df680d6240e2dc8
Ipsit <ishukr2101@gmail.com>
2022-06-10 23:06:28 +0530

    logic

--- src/My Components/Board.js
+++ src/My Components/Board.js
@@ -4,25 +4,43 @@ import { useState } from "react";
 
 const Board = () => {
 
-  const [state, setState] = useState(Array(9).fill(null));
+  const [state, setState] = useState(Array(9).fill(''));
+  const [isXNext, setXNext] = useState(false);
   console.log(state); 
+
+  const updateCell = (position) => {
+    if (state[position] !== '') return;   // if value at that position already exists
+
+    setState((prev) => {
+      return prev.map((square, pos) => {
+
+        if (pos === position) {
+          return (isXNext ? 'X' : 'O');
+        }
+        else return square;
+
+      });
+    });
+
+    setXNext((prev) => !prev);
+  }
 
   return (
     <div className="board">
       <div className="board-row">
-        <Square value={state} />
-        <Square value={state} />
-        <Square value={state} />
+        <Square onClick = {() => { updateCell(0) }} value = {state[0]}/>
+        <Square onClick = {() => { updateCell(1) }} value = {state[1]} />
+        <Square onClick = {() => { updateCell(2) }} value = {state[2]} />
       </div>
       <div className="board-row">
-        <Square value={state} />
-        <Square value={state} />
-        <Square value={state} />
+        <Square onClick = {() => { updateCell(3) }} value = {state[3]} />
+        <Square onClick = {() => { updateCell(4) }} value = {state[4]} />
+        <Square onClick = {() => { updateCell(5) }} value = {state[5]} />
       </div>
       <div className="board-row">
-        <Square value={state} />
-        <Square value={state} />
-        <Square value={state} />
+        <Square onClick = {() => { updateCell(6) }} value = {state[6]} />
+        <Square onClick = {() => { updateCell(7) }} value = {state[7]} />
+        <Square onClick = {() => { updateCell(8) }} value = {state[8]} />
       </div>
     </div>
   );
--- src/My Components/Square.js
+++ src/My Components/Square.js
@@ -1,9 +1,10 @@
 import React from 'react'
 
-const Square = ({value}) => {
+const Square = (props) => {
+
   return (
     <>
-      <button type = "button" className='square'> {value} </button>
+      <button type = "button" className='square' onClick={props.onClick}> {props.value} </button>
     </>
   );
 }
